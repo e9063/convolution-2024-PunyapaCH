743020cf84895aadc7e6d2839d4125a72a6c7554
PunyapaCH <160213666+PunyapaCH@users.noreply.github.com>
2024-10-30 15:49:52 +0000

    Add new file for assignment

---
+++ conv_sequential.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Sequential
+
+int main(){
+    // ---- input and malloc A, F ----
+    int NA, NF;
+    scanf("%d %d", &NA, &NF);
+    int *A = malloc(sizeof(int) * NA);
+    int *F = malloc(sizeof(int) * NF);
+    int *FF = malloc(sizeof(int) * NF);
+
+    int NR = NA - NF + 1;
+    int *arr = malloc(sizeof(int) * NR);
+
+    for(int i = 0; i < NA; i++){
+        scanf("%d", &A[i]);
+    }
+    for(int i = 0; i < NF; i++){
+        scanf("%d", &F[i]);
+    }
+
+    for (int i = 0 ; i < NF ; i++) {
+        FF[i] = F[NF-i-1];
+    }
+
+    // ---- end input and malloc----
+
+    // implement here
+
+    for (int k = 0; k < NR; k++) {
+        int sum = 0;
+        for (int i = 0; i < NF; i++) {
+            sum += A[k + i] * FF[i];
+        }
+        arr[k] = sum;
+    }
+
+
+    // Print the result
+    //for (int i = 0; i < NR; i++) {
+        //printf("%d\n", arr[i]);
+    //}
+
+
+    // ---- free memory ----
+    free(FF);
+    free(F);
+    free(A);
+    free(arr);
+    // ---- end free ----
+    return 0;
+}
